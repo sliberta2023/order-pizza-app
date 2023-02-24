fa869a168fe6eb0779d1e0314a706963b0e4ba35
Solomon Liberta <solomon.liberta@postclick.com>
2023-02-24 01:12:46 -0600

    feat: finalized order creation & details flow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,7 @@ const routes: Routes = [
     canActivate: [AuthGuardService]
   },
   {
-    path: 'details/:id',
+    path: 'details/:orderId',
     component: PageOrderDetails,
     canActivate: [AuthGuardService]
   },
